995cd2b6f10cd9a1edf9e25e98f4f74f250fc12c
Genifer Moreno <genifer1105@gmail.com>
2019-07-13 09:29:00 -0500

    dismiss function has been added on station comments page

--- src/pages/station-comments/station-comments.ts
+++ src/pages/station-comments/station-comments.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
 /**
  * Generated class for the StationCommentsPage page.
@@ -44,11 +44,19 @@ export class StationCommentsPage {
     }
   ]
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private viewCtrl: ViewController
+    ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad StationCommentsPage');
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
   }
 
 }
